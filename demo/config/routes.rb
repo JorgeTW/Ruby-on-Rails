9e711d6baac263bdd8e6b4c2afde023bd7651392
Jorge <Jorge0429.chen@gmail.com>
2018-07-11 11:58:56 +0800

    轉址功能

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :users
   #get "/", to: "users#index"
   root "users#index"
+  get "/test_redirect_users", to: redirect('/users')
+  get "/test_redirect_users1", to: redirect('users#index')
+  get "/test_redirect_users_new", to: redirect('users#new')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
