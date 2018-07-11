01195d2d9d79175e4c2a36679eca86c80fb841ad
Jorge <Jorge0429.chen@gmail.com>
2018-07-11 12:48:32 +0800

    控制路由數目

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   resources :tests#建立8個路由
+  resources :tests1, only: [:index, :show]#建立2個路由, index & show
+  resources :tests1, except: [:new, :create, :edit, :update, :destroy]#刪除其他路由, 只剩2個, index & show
   #get "/", to: "users#index"
   root "users#index"
   get "/test_redirect_users", to: redirect('/users')
